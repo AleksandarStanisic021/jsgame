4907ab33d87a26717967f0ba46d646572a92e79f
ciandre1976 <ciandre23@gmail.com>
2026-03-04 19:28:34 +0100

    fixing bugs

--- ,/src/main.js
+++ ,/src/main.js
@@ -20,12 +20,49 @@ class Raven {
     this.y = Math.random() * (canvas.height - this.height);
     this.directionX = Math.random() * 5 + 3;
     this.directionY = Math.random() * 5 - 2.5;
+    this.markedForDeletion = false;
+    this.image = new Image();
+    this.image.src = raven;
+    this.spriteWidth = 271;
+    this.spriteHeight = 194;
+    this.frame = 0;
+    this.maxFrame = 4;
+    this.timeSinceFlap = 0;
+    this.flapInterval = Math.random() * 100 + 100;
   }
-  update() {
+
+  update(deltatime) {
+    if (this.x < 0 - this.width) this.markedForDeletion = true;
+
+    this.timeSinceFlap += deltatime;
+    if (this.timeSinceFlap > this.flapInterval) {
+      if (this.frame > this.maxFrame) this.frame = 0;
+      else this.frame++;
+      this.timeSinceFlap = 0;
+    }
+
+    this.x -= this.directionX;
+    this.y += this.directionY;
+    if (this.y > canvas.height - this.height)
+      this.directionY = -Math.random() * 5 - 2.5;
+    if (this.y < 0) this.directionY = Math.random() * 5 - 2.5;
+    if (this.x < 0 - this.width) this.directionX = Math.random() * 5 + 3;
+    if (this.x > canvas.width) this.directionX = -Math.random() * 5 - 3;
+
     this.x -= this.directionX;
   }
   draw() {
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      this.image,
+      this.frame * this.spriteWidth,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      (this.width = this.spriteWidth / 2),
+      (this.height = this.spriteHeight / 2),
+    );
   }
 }
 
@@ -42,12 +79,13 @@ function animate(timestamp) {
     timeToNextRaven = 0;
   }
   [...ravens].forEach((raven) => {
-    raven.update();
+    raven.update(deltaTime);
   });
   [...ravens].forEach((raven) => {
     raven.draw();
   });
-
+  ravens = ravens.filter((raven) => !raven.markedForDeletion);
+  console.log(ravens);
   requestAnimationFrame(animate);
 }
 animate(0);
